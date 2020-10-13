5c75c8d6fc51caa4744bc9f551ae707c93e7e33b
nathanmckenzie <ngm18@acu.edu>
2020-10-13 14:28:36 -0500

    Initial Commit

---
+++ SwiftyDice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftyDice
+//
+//  Created by Nathan Mckenzie on 10/13/20.
+//  Copyright © 2020 Nathan Mckenzie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
